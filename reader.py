cf0e372540c1839fd8dc25d96537d04b5fbf3723
tamaricoh <106402042+tamaricoh@users.noreply.github.com>
2022-09-04 16:44:49 +0300

    Create a class named 'reader'

---
+++ reader.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+class reader :
+
+    def __init__(self) :
+        self.id = 0
+        self.name = ""
+        self.books = [] # Each cell is { "bookTitle" : "XXX" , "dateOfBorrowing" : "YYYY-MM-DD"}
+    
+    def read_book(self, title) :
+        date = str(datetime.now().date())
+        bookBorrowed = { "bookTitle" : title , "dateOfBorrowing" : date}
+        self.books.append(bookBorrowed)
+    
+
