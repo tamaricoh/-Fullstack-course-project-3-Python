bf670900e1ac6d32c1496cda4034cbfa984f12c7
tamaricoh <106402042+tamaricoh@users.noreply.github.com>
2022-09-08 17:00:51 +0300

    Adding new function - 'to_json' in 'book'

--- book.py
+++ book.py
@@ -5,4 +5,10 @@ class book :
         self.title = ""
         self.num_of_pages = 0
     
+    def to_json(self) :
+        return {
+            "author" : self.author,
+            "title" : self.title,
+            "num_of_pages" : self.num_of_pages
+        }
     
